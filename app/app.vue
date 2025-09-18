e694ead18d4e7b55a9de62bb6917558f4e6fadbd
Max <maximilien01993@gmail.com>
2025-09-18 16:09:56 +0200

    init home view

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
+<script setup lang="ts">
+  import Home from './Views/Home.vue';
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <Home />
 </template>
