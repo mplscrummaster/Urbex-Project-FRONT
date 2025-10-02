9819214daeec9082efd3f40aa6d245a85ade6375
Max <maximilien01993@gmail.com>
2025-10-02 13:42:37 +0200

    refactor GlitchClock component for improved readability and structure

--- src/components/GlitchClock.vue
+++ src/components/GlitchClock.vue
@@ -1,47 +1,45 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
+  import { ref, onMounted, onUnmounted } from "vue";
 
-const time = ref("");
-const isOff = ref(true);
-const isGlitch = ref(true);
+  const time = ref("");
+  const isOff = ref(true);
+  const isGlitch = ref(true);
 
-let intervalId;
-let animIntervalId;
+  let intervalId;
+  let animIntervalId;
 
-onMounted(() => {
-  // clock startup
-  setTimeout(() => {
-    isOff.value = false;
-
-  }, 2000);
-
-  // real-time clock update
-  intervalId = setInterval(() => {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
-    time.value = `${hours} : ${minutes} : ${seconds}`;
-  }, 1000);
-
-  animIntervalId = setInterval(() => {
-    isOff.value = true;
+  onMounted(() => {
+    // clock startup
     setTimeout(() => {
       isOff.value = false;
-    }, 2000); // тривалість анімації
-  }, 4000);
-});
 
+    }, 2000);
 
+    // real-time clock update
+    intervalId = setInterval(() => {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const seconds = now.getSeconds().toString().padStart(2, "0");
+      time.value = `${hours} : ${minutes} : ${seconds}`;
+    }, 1000);
 
-onUnmounted(() => {
-  clearInterval(intervalId);
-  clearInterval(animIntervalId);
-});
+    animIntervalId = setInterval(() => {
+      isOff.value = true;
+      setTimeout(() => {
+        isOff.value = false;
+      }, 2000); // тривалість анімації
+    }, 4000);
+  });
 
-function toggleGlitch() {
-  isGlitch.value = !isGlitch.value;
-}
+  onUnmounted(() => {
+    clearInterval(intervalId);
+    clearInterval(animIntervalId);
+  });
+
+  function toggleGlitch() {
+    isGlitch.value = !isGlitch.value;
+  }
 </script>
 
 <template>
@@ -55,317 +53,318 @@ function toggleGlitch() {
 </template>
 
 <style scoped lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Lato:400,700");
+  @import url("https://fonts.googleapis.com/css?family=Lato:400,700");
 
-$bg-color: #11111178;
-$txt-color: #fff;
+  $bg-color: #11111178;
+  $txt-color: #fff;
 
-$color-c1: red;
-$color-c2: blue;
+  $color-c1: red;
+  $color-c2: blue;
 
-$offset-c1: 3;
-$offset-c2: 2;
+  $offset-c1: 3;
+  $offset-c2: 2;
 
-$lay-c1: 2;
-$lay-c2: 2;
+  $lay-c1: 2;
+  $lay-c2: 2;
 
-@import url("https://fonts.googleapis.com/css?family=Lato:400,700");
+  @import url("https://fonts.googleapis.com/css?family=Lato:400,700");
 
-$bg-color: #11111178;
-$txt-color: #fff;
+  $bg-color: #11111178;
+  $txt-color: #fff;
 
-$color-c1: red;
-$color-c2: blue;
+  $color-c1: red;
+  $color-c2: blue;
 
-$offset-c1: 3;
-$offset-c2: 2;
+  $offset-c1: 3;
+  $offset-c2: 2;
 
-$lay-c1: 2;
-$lay-c2: 2;
+  $lay-c1: 2;
+  $lay-c2: 2;
 
-/* RESET */
-*,
-*:before,
-*:after {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  outline: 0;
-  box-sizing: border-box;
-  user-select: none;
-}
-
-html,
-body {
-  height: 100%;
-}
-
-body {
-  font-family: "Lato", sans-serif;
-  font-weight: 700;
-  background: $bg-color;
-  color: $txt-color;
-}
-
-/* SWITCHER BUTTON */
-a.switcher {
-  display: block;
-  position: fixed;
-  text-decoration: none;
-  z-index: 9999;
-  right: 20px;
-  bottom: 20px;
-  width: 16px;
-  height: 16px;
-  background: transparent;
-  border: 2px solid $txt-color;
-  border-radius: 50%;
-  opacity: 0.15;
-  transition: opacity 0.15s;
-
-  &:hover {
-    opacity: 1;
+  /* RESET */
+  *,
+  *:before,
+  *:after {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    box-sizing: border-box;
+    user-select: none;
   }
 
-  &:before {
-    content: "";
-    position: absolute;
-    width: 2px;
-    height: 5px;
-    border-radius: 2px;
-    background: $txt-color;
-    top: 0;
-    left: 5px;
+  html,
+  body {
+    height: 100%;
   }
-}
 
-/* SCREEN */
-.screen {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-}
+  body {
+    font-family: "Lato", sans-serif;
+    font-weight: 700;
+    background: $bg-color;
+    color: $txt-color;
+  }
 
-/* CLOCK */
-.clock {
-  position: relative;
-  width: 90%; // адаптивна ширина
-  max-width: 720px;
-  text-align: center;
-  cursor: default;
-
-  span {
+  /* SWITCHER BUTTON */
+  a.switcher {
     display: block;
-    font-size: clamp(48px, 15vw, 128px); // адаптивний шрифт
-    line-height: 1;
-    position: relative;
-  }
-
-  &.is-off {
-    animation: is-off 2s linear infinite !important;
-  }
-}
-
-/* GLITCH EFFECT */
-.glitch {
-  &:before {
-    position: absolute;
+    position: fixed;
+    text-decoration: none;
     z-index: 9999;
-    content: "";
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    animation: bg-move 2s linear infinite;
-    background-size: 100% 8px;
+    right: 20px;
+    bottom: 20px;
+    width: 16px;
+    height: 16px;
+    background: transparent;
+    border: 2px solid $txt-color;
+    border-radius: 50%;
+    opacity: 0.15;
+    transition: opacity 0.15s;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    &:before {
+      content: "";
+      position: absolute;
+      width: 2px;
+      height: 5px;
+      border-radius: 2px;
+      background: $txt-color;
+      top: 0;
+      left: 5px;
+    }
   }
 
+  /* SCREEN */
+  .screen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    margin-block-start: 200px;
+  }
+
+  /* CLOCK */
   .clock {
-    transform: skewX(0deg) scaleY(1);
-    animation: clock-bag 4s linear infinite;
+    position: relative;
+    width: 90%; // адаптивна ширина
+    max-width: 720px;
+    text-align: center;
+    cursor: default;
 
     span {
+      display: block;
+      font-size: clamp(48px, 15vw, 128px); // адаптивний шрифт
+      line-height: 1;
+      position: relative;
+    }
 
-      &:before,
-      &:after {
-        content: attr(data-time);
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: $bg-color;
-        color: $txt-color;
-        overflow: hidden;
-        clip: rect(0, 9999px, 0, 0);
-      }
+    &.is-off {
+      animation: is-off 2s linear infinite !important;
+    }
+  }
 
-      &:before {
-        left: -$offset-c2;
-        text-shadow: $lay-c2 0 $color-c2;
-        animation: c2 1s infinite linear alternate-reverse;
-      }
+  /* GLITCH EFFECT */
+  .glitch {
+    &:before {
+      position: absolute;
+      z-index: 9999;
+      content: "";
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      animation: bg-move 2s linear infinite;
+      background-size: 100% 8px;
+    }
 
-      &:after {
-        left: $offset-c1;
-        text-shadow: -$lay-c1 0 $color-c1;
-        animation: c1 2s infinite linear alternate-reverse;
+    .clock {
+      transform: skewX(0deg) scaleY(1);
+      animation: clock-bag 4s linear infinite;
+
+      span {
+
+        &:before,
+        &:after {
+          content: attr(data-time);
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: $bg-color;
+          color: $txt-color;
+          overflow: hidden;
+          clip: rect(0, 9999px, 0, 0);
+        }
+
+        &:before {
+          left: -$offset-c2;
+          text-shadow: $lay-c2 0 $color-c2;
+          animation: c2 1s infinite linear alternate-reverse;
+        }
+
+        &:after {
+          left: $offset-c1;
+          text-shadow: -$lay-c1 0 $color-c1;
+          animation: c1 2s infinite linear alternate-reverse;
+        }
       }
     }
   }
-}
 
-/* ANIMATIONS */
-@keyframes is-off {
+  /* ANIMATIONS */
+  @keyframes is-off {
 
-  0%,
-  50% {
-    opacity: 1;
+    0%,
+    50% {
+      opacity: 1;
+    }
+
+    56%,
+    57% {
+      opacity: 0;
+    }
+
+    58% {
+      opacity: 1;
+    }
+
+    71% {
+      transform: scaleY(1) skewX(0deg);
+    }
+
+    72% {
+      transform: scaleY(3) skewX(-60deg);
+    }
+
+    73% {
+      transform: scaleY(1) skewX(0deg);
+    }
+
+    80% {
+      opacity: 1;
+    }
+
+    81%,
+    84% {
+      opacity: 0;
+    }
+
+    85% {
+      opacity: 1;
+    }
+
+    91% {
+      transform: scaleX(1) scaleY(1) skewX(0deg);
+      color: $txt-color;
+    }
+
+    92% {
+      transform: scaleX(1.5) scaleY(.2) skewX(80deg);
+      color: green;
+    }
+
+    93% {
+      transform: scaleX(1) scaleY(1) skewX(0deg);
+      color: $txt-color;
+    }
   }
 
-  56%,
-  57% {
-    opacity: 0;
+  @keyframes c1 {
+    0% {
+      clip: rect(12px, 9999px, 85px, 0);
+    }
+
+    20% {
+      clip: rect(50px, 9999px, 70px, 0);
+    }
+
+    40% {
+      clip: rect(10px, 9999px, 90px, 0);
+    }
+
+    60% {
+      clip: rect(45px, 9999px, 100px, 0);
+    }
+
+    80% {
+      clip: rect(20px, 9999px, 60px, 0);
+    }
+
+    100% {
+      clip: rect(0px, 9999px, 80px, 0);
+    }
   }
 
-  58% {
-    opacity: 1;
+  @keyframes c2 {
+    0% {
+      clip: rect(15px, 9999px, 60px, 0);
+    }
+
+    25% {
+      clip: rect(30px, 9999px, 100px, 0);
+      transform: scaleX(.9);
+    }
+
+    50% {
+      clip: rect(5px, 9999px, 75px, 0);
+    }
+
+    75% {
+      clip: rect(40px, 9999px, 85px, 0);
+    }
+
+    100% {
+      clip: rect(0px, 9999px, 100px, 0);
+    }
   }
 
-  71% {
-    transform: scaleY(1) skewX(0deg);
+  @keyframes clock-bag {
+    0% {
+      transform: translate(0, 0);
+    }
+
+    25% {
+      transform: translate(2px, -2px);
+    }
+
+    50% {
+      transform: translate(-2px, 2px);
+    }
+
+    75% {
+      transform: translate(3px, -1px);
+    }
+
+    100% {
+      transform: translate(0, 0);
+    }
   }
 
-  72% {
-    transform: scaleY(3) skewX(-60deg);
+  @keyframes bg-move {
+    0% {
+      background-position: 0 0;
+    }
+
+    100% {
+      background-position: 0 -32px;
+    }
   }
 
-  73% {
-    transform: scaleY(1) skewX(0deg);
-  }
+  /* RESPONSIVE MEDIA QUERIES */
+  @media (max-width: 480px) {
+    .clock span {
+      font-size: clamp(36px, 20vw, 80px);
+    }
 
-  80% {
-    opacity: 1;
+    .glitch .clock span:before,
+    .glitch .clock span:after {
+      text-shadow: none; // менш агресивний глітч на мобільних
+    }
   }
-
-  81%,
-  84% {
-    opacity: 0;
-  }
-
-  85% {
-    opacity: 1;
-  }
-
-  91% {
-    transform: scaleX(1) scaleY(1) skewX(0deg);
-    color: $txt-color;
-  }
-
-  92% {
-    transform: scaleX(1.5) scaleY(.2) skewX(80deg);
-    color: green;
-  }
-
-  93% {
-    transform: scaleX(1) scaleY(1) skewX(0deg);
-    color: $txt-color;
-  }
-}
-
-@keyframes c1 {
-  0% {
-    clip: rect(12px, 9999px, 85px, 0);
-  }
-
-  20% {
-    clip: rect(50px, 9999px, 70px, 0);
-  }
-
-  40% {
-    clip: rect(10px, 9999px, 90px, 0);
-  }
-
-  60% {
-    clip: rect(45px, 9999px, 100px, 0);
-  }
-
-  80% {
-    clip: rect(20px, 9999px, 60px, 0);
-  }
-
-  100% {
-    clip: rect(0px, 9999px, 80px, 0);
-  }
-}
-
-@keyframes c2 {
-  0% {
-    clip: rect(15px, 9999px, 60px, 0);
-  }
-
-  25% {
-    clip: rect(30px, 9999px, 100px, 0);
-    transform: scaleX(.9);
-  }
-
-  50% {
-    clip: rect(5px, 9999px, 75px, 0);
-  }
-
-  75% {
-    clip: rect(40px, 9999px, 85px, 0);
-  }
-
-  100% {
-    clip: rect(0px, 9999px, 100px, 0);
-  }
-}
-
-@keyframes clock-bag {
-  0% {
-    transform: translate(0, 0);
-  }
-
-  25% {
-    transform: translate(2px, -2px);
-  }
-
-  50% {
-    transform: translate(-2px, 2px);
-  }
-
-  75% {
-    transform: translate(3px, -1px);
-  }
-
-  100% {
-    transform: translate(0, 0);
-  }
-}
-
-@keyframes bg-move {
-  0% {
-    background-position: 0 0;
-  }
-
-  100% {
-    background-position: 0 -32px;
-  }
-}
-
-/* RESPONSIVE MEDIA QUERIES */
-@media (max-width: 480px) {
-  .clock span {
-    font-size: clamp(36px, 20vw, 80px);
-  }
-
-  .glitch .clock span:before,
-  .glitch .clock span:after {
-    text-shadow: none; // менш агресивний глітч на мобільних
-  }
-}
 </style>
